63ee434060525738cc0cd3f8059ee88978d1f4ad
MrYellowSock <hahaunji@gmail.com>
2024-07-26 15:56:30 +0700

    update

--- client/client.py
+++ client/client.py
@@ -5,9 +5,7 @@ import requests
 import time
 
 time.sleep(5)
-
 RAY_ADDR = os.getenv('RAY_HEAD_DASHBOARD_ADDRESS')
-FILE_SERVER_ADDR = os.getenv('FILE_SERVER_ADDRESS')
 
 url = f"http://{RAY_ADDR}/api/serve/applications/"
 headers = {"Accept": "application/json", "Content-Type": "application/json"}
@@ -16,18 +14,24 @@ headers = {"Accept": "application/json", "Content-Type": "application/json"}
 data ={
     "applications": [
         {
-            "name": "deployment1",
+            "name": "MyModelDeployment",
             "route_prefix": "/",
             "import_path": "code:app",
             "runtime_env": {
                 # "working_dir": "https://github.com/ray-project/serve_config_examples-master.zip"
                 # "working_dir": "s3://tisco.alpha.data.analytics/test_ray_working_dir/test.zip",
                 # "working_dir": f"file://{os.path.abspath('text_ml.zip')}"
-                "working_dir": f"http://{FILE_SERVER_ADDR}/deployment1/deployment1.zip",
-                "pip":[ "-r" ,"requirements.txt" ] 
+                "working_dir": f"https://github.com/MrYellowSock/ray-test/raw/main/deployments/deployment1/deployment1.zip",
+
+                # using requirements.txt not gonna work
+                # https://discuss.ray.io/t/requirements-txt-support-with-job-api/6338/6
+
+                "pip":{
+                    "packages":["pandas==2.2.1"],
+                }
             },
             "deployments": [
-                {"name": "deployment1"},
+                {"name": "MyModelDeployment"},
             ]
         },
     ]
@@ -37,3 +41,4 @@ response = requests.put(url, headers=headers, data=json.dumps(data))
 
 if response.status_code != 200:
     raise Exception(f"Failed to create or update application: {response.text}")
+print("DEPLOYED")
--- deployments/deployment1/code.py
+++ deployments/deployment1/code.py
@@ -1,31 +1,21 @@
-import pandas as pd
-from fastapi import FastAPI
+import requests
+from starlette.requests import Request
+from typing import Dict
 from ray import serve
-import io
 
-# 1: Define a FastAPI app and wrap it in a deployment with a route handler.
-app = FastAPI()
+@serve.deployment
+class MyModelDeployment:
+    def __init__(self, msg: str):
 
+        self._msg = msg
 
-@serve.deployment(
-    name="deployment1",
-    num_replicas=2,
-    ray_actor_options={"num_cpus": 2, "num_gpus": 0},
-    max_ongoing_requests=100,
-    health_check_period_s=10,
-    health_check_timeout_s=30,
-    graceful_shutdown_timeout_s=20,
-    graceful_shutdown_wait_loop_s=2,
-)
-@serve.ingress(app)
-class FastAPIDeployment:
-    def __init__(self):
-        # get bigmodel file size
-        with open("deployment1/bigmodel.pkl", "rb") as f:
-            self.bigmodel = io.BytesIO(f.read())
+    def __call__(self, request: Request) -> Dict:
+        import os
+        import pandas as pd
+        return {
+                "result": self._msg,
+                "hostname": os.uname()[1],
+                "version":pd.__version__
+        }
 
-    # FastAPI will automatically parse the HTTP request for us.
-    @app.get("/hello")
-    def say_hello(self, name: str) -> str:
-        return f"Hello {name}! with {pd.__version__} {self.bigmodel.getbuffer().nbytes}"
-
+app = MyModelDeployment.bind(msg="Hello world!")
