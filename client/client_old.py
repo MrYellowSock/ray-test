809f8d12323e1a34b56bea2719cc1e18dc0cd372
MrYellowSock <hahaunji@gmail.com>
2024-07-30 16:28:34 +0700

    finally

--- client/client_old.py
+++ client/client_old.py
@@ -10,35 +10,48 @@ RAY_ADDR = os.getenv('RAY_HEAD_DASHBOARD_ADDRESS')
 url = f"http://{RAY_ADDR}/api/serve/applications/"
 headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
-
-data ={
+# STORE MODELS IN GIT?
+# Corrupt zip file can cause freeze?
+data = {
     "applications": [
-        {
-            "name": "whatthe",
-            "route_prefix": "/mymodel",
-            "import_path": "code:app",
+         {
+            "name": "sleep1",
+            "route_prefix": "/sleep1",
+            "import_path": "sleepy_pid:app",
             "runtime_env": {
-                # "working_dir": "https://github.com/ray-project/serve_config_examples-master.zip"
-                # "working_dir": "s3://tisco.alpha.data.analytics/test_ray_working_dir/test.zip",
-                # "working_dir": f"file://{os.path.abspath('text_ml.zip')}"
-                "working_dir": f"https://github.com/MrYellowSock/ray-test/raw/main/deployments/deployment1/deployment1.zip",
-
-                # using requirements.txt not gonna work
-                # https://discuss.ray.io/t/requirements-txt-support-with-job-api/6338/6
-
-                # "pip":{
-                #     "packages":["pandas==2.2.1"],
-                # }
+                "working_dir": "s3://wahthecufk/ray-test-sampleconfig-997702ffa1315db0f5c9c679d8d6a2f9f43c7b63.zip"
             },
             "deployments": [
-                {
-                    "name": "MyModelDeployment",
-                                        "route_prefix": "/"
-                    },
+                {"name": "SleepyPid"}
             ]
-        },
+        }
+         ,{
+            "name": "sleep2",
+            "route_prefix": "/sleep2",
+            "import_path": "sleepy_pid:app",
+            "runtime_env": {
+                # SOME HOW USING URL FOR RAW FILE DOES NOT WORK??
+                "working_dir": "https://github.com/MrYellowSock/ray-test-sampleconfig/archive/HEAD.zip"
+            },
+            "deployments": [
+                {"name": "SleepyPid"}
+            ]
+        }
+         ,{
+            "name": "myfastapi",
+            "route_prefix": "/myfastapi",
+            "import_path": "fastapi_sample:app",
+            "runtime_env": {
+                # SOME HOW USING URL FOR RAW FILE DOES NOT WORK??
+                "working_dir": "https://github.com/MrYellowSock/ray-test-sampleconfig/archive/HEAD.zip"
+            },
+            "deployments": [
+                {"name": "MyModelDeployment"}
+            ]
+        }
     ]
 }
+
 
 response = requests.put(url, headers=headers, data=json.dumps(data))
 
