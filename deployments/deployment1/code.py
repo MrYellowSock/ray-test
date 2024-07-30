cde3f146e40f798fdd405cd30dd75878a7611ed8
MrYellowSock <hahaunji@gmail.com>
2024-07-30 13:30:18 +0700

    update zip

--- deployments/deployment1/code.py
+++ deployments/deployment1/code.py
@@ -1,22 +1,27 @@
 from fastapi import FastAPI
 from typing import Dict
 from ray import serve
+import ray
 
-api = FastAPI()
+ray.init(address="auto", namespace="serve")
+serve.start(detached=True)
+
+app = FastAPI()
 @serve.deployment
-@serve.ingress(api)
+@serve.ingress(app)
 class MyModelDeployment:
     def __init__(self, msg: str):
 
         self._msg = msg
 
-    @api.get("/")
+    @app.get("/")
     def root(self) -> Dict:
         import os
         import pandas as pd
         return {"result": self._msg, "hostname": os.uname()[1], "pd_version":pd.__version__}
 
-    @api.post("/ping")
+    @app.post("/ping")
     def ping(self,name:str) -> Dict:
         return {"result": f"pong {name}"}
-app = MyModelDeployment.bind(msg="Hello world!")
+
+MyModelDeployment.deploy()
